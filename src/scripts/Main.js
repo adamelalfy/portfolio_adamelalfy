aceebfb508092d54e130dea94b07a8aa8ff912f8
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2025-10-25 14:47:49 -0400

    formulairephp (desc)
    added class/gbi_portfolio.class.php
    added phpmailer folder

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,5 +1,5 @@
-import ComponentFactory from "./ComponentFactory.js";
-import Icons from "./utils/Icons.js";
+import ComponentFactory from './ComponentFactory.js';
+// import Icons from "./utils/Icons.js";
 
 class Main {
   constructor() {
@@ -7,11 +7,11 @@ class Main {
   }
 
   init() {
-    document.documentElement.classList.add("has-js");
+    document.documentElement.classList.add('has-js');
 
     new ComponentFactory();
 
-    Icons.load();
+    // Icons.load();
   }
 }
 new Main();
